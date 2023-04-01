2c86f6d79e0aef9c2a53b6187e03ade6aeba41d0
Nezaket Bağlam <180101017@ogrenci.yalova.edu.tr>
2023-04-01 03:28:11 +0300

    minor changes and navbar added

---
+++ DemansAppWeb/Controllers/CommandController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemansAppWeb.Controllers
+{
+    public class CommandController : Controller
+    {
+        private readonly ILogger<CommandController> _logger;
+
+        public CommandController(ILogger<CommandController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
